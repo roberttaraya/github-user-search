bc02486fb0fafb43fc053362d5b90ed1ef3a81b1
roberttaraya <robert.taraya@gmail.com>
2018-06-14 03:30:01 -0500

    remove debugging comments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,17 +2,5 @@ class HomeController < ApplicationController
   before_action :require_authenticated_user
 
   def index
-    # session[:access_token] = nil
-    puts "*****HomeController:index"
-    puts "*****session[:access_token] = #{session[:access_token]}"
-    if authenticated?
-      puts "*****HomeController: i'm authenticated now"
-      puts "*****session[:access_token] = #{session[:access_token]}"
-      @message = "*****HomeController: i'm authenticated now"
-    else
-      puts "*****HomeController: i'm not authenticated now"
-      puts "*****session[:access_token] = #{session[:access_token]}"
-      @message = "*****HomeController: i'm NOT authenticated now"
-    end
   end
 end
